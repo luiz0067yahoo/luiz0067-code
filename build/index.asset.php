cc5f6d3b2c79830a57bc65ca4d4a7323a8b48ba1
@luiz0067yahoo <102493028+luiz0067yahoo@users.noreply.github.com>
2026-09-14 18:12:18 -0300

    fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'acbce0fbe6aa78e4df2a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1c556b5b1494604d1436');
